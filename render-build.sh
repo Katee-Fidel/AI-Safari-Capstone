c448be75e41626ea7da863c3b556b2b2842e7879
iguanafrm23rd <kateefidel@gmail.com>
2026-06-05 09:14:19 +0300

    Render: fix pip flags in build script

--- render-build.sh
+++ render-build.sh
@@ -5,7 +5,7 @@ python -m pip install --upgrade pip
 
 # Install using binary-first strategy.
 # Do NOT use --only-binary=:all: because Render/Python may not have wheels for every pinned version.
-python -m pip install --only-binary=:all: -r requirements.render.txt --no-deps
+python -m pip install --only-binary=:all: -r requirements.render.txt
 
 
 
