64581567454f5c2964fba30bd6c7d4044d59456e
iguanafrm23rd <kateefidel@gmail.com>
2026-06-05 12:56:36 +0300

    fix: remove --only-binary flag blocking crewai 1.x install

--- render-build.sh
+++ render-build.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env sh
 set -eu
 
-python -m pip install --upgrade pip
+python -m pip install --upgrade pip setuptools wheel
 
-# Install using binary-first strategy.
-# Do NOT use --only-binary=:all: because Render/Python may not have wheels for every pinned version.
-python -m pip install --only-binary=:all: -r requirements.render.txt
+pip install -r requirements.render.txt
 
-
-
-
+python manage.py collectstatic --no-input
+python manage.py migrate
