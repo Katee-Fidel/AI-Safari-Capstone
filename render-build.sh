35fa7ce35c376c617dc8bdc18b3e1f6053bc9ffb
iguanafrm23rd <kateefidel@gmail.com>
2026-06-11 13:12:05 +0300

    fix: move migrate to build script for free tier

--- render-build.sh
+++ render-build.sh
@@ -5,5 +5,6 @@ python -m pip install --upgrade pip setuptools wheel
 
 pip install -r requirements.render.txt
 
+DJANGO_SETTINGS_MODULE=config.settings.production python manage.py migrate --no-input
 
 DJANGO_SETTINGS_MODULE=config.settings.production python manage.py collectstatic --no-input
